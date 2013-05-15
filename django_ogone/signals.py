2538d28f161182040ef65b878fd9e1548b290c8b
Venkata Ramana <ramana@agiliq.com>
2013-05-15 18:26:42 +0530

    add failed and cancelled signals with default tr feedback params

--- django_ogone/signals.py
+++ django_ogone/signals.py
@@ -1,4 +1,10 @@
 from django.dispatch import Signal
 
 ogone_payment_accepted = Signal(providing_args=['order_id', \
-                                                'amount', 'currency'])
+                                                'amount', 'currency', 'pay_id', 'status', 'ncerror'])
+ogone_payment_failed = Signal(providing_args=['order_id', \
+                                                'amount', 'currency', 'pay_id', 'status', 'ncerror'])
+ogone_payment_cancelled = Signal(providing_args=['order_id', \
+                                                'amount', 'currency', 'pay_id', 'status', 'ncerror'])
+
+
